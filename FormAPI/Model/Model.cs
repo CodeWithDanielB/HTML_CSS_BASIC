166ed853a742df0369556280d19a3bfcfcae3d0a
CodeWithDanielB <danielbiswakarma24@gmail.com>
2024-03-06 15:25:29 +0545

    added the script for console json

---
+++ FormAPI/Model/Model.cs
@@ -0,0 +1,10 @@
+﻿namespace FormAPI.Model
+{
+    public class Model
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string EMail { get; set; }
+        public string address { get; set; }
+    }
+}
