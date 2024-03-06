60463fbbd9f26c0f1e5db305ea957e6481a73278
CodeWithDanielB <danielbiswakarma24@gmail.com>
2024-03-06 16:39:07 +0545

    adding the backend in c#

---
+++ FormAPI/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/form/details")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var formData = new FormAPI.Model.Model();
+
+
+            formData.first_name = Request.Query["first_name"];
+            formData.last_name = Request.Query["last_name"];
+            formData.EMail = Request.Query["E_Mail"];
+            formData.address = Request.Query["address"];
+
+
+            Console.WriteLine(formData.first_name);
+
+            return NoContent();
+        }
+    }
+}
--- FormAPI/obj/Debug/net8.0/FormAPI.AssemblyInfo.cs
+++ FormAPI/obj/Debug/net8.0/FormAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa6bfe18796971d561aa7fb3495ff342cffff9f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+166ed853a742df0369556280d19a3bfcfcae3d0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
